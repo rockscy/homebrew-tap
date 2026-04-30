e51f37ae895cfad25ff1ed11504b59e9d5a46b30
rockscy <rockscy@users.noreply.github.com>
2026-04-30 19:26:37 +0800

    spell 0.2.1: fix init wizard key concatenation bug

--- Formula/spell.rb
+++ Formula/spell.rb
@@ -1,28 +1,28 @@
 class Spell < Formula
   desc "AI command palette for your terminal"
   homepage "https://github.com/rockscy/spell"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/rockscy/spell/releases/download/v0.2.0/spell_0.2.0_darwin_arm64.tar.gz"
-      sha256 "b0a7482e9ae1b120fc2811cc1fb38fcf49edd4b8ec3afae6d8559146fe64f496"
+      url "https://github.com/rockscy/spell/releases/download/v0.2.1/spell_0.2.1_darwin_arm64.tar.gz"
+      sha256 "f086a46f3939ace79f8b6c71293c9839b47d9cd65b97a6e8418f71fce3b57d97"
     end
     on_intel do
-      url "https://github.com/rockscy/spell/releases/download/v0.2.0/spell_0.2.0_darwin_amd64.tar.gz"
-      sha256 "fedf6d80a86b411682a3c1d78f8febb6f1e9cf0f1a1c8f8cb536921005629ae8"
+      url "https://github.com/rockscy/spell/releases/download/v0.2.1/spell_0.2.1_darwin_amd64.tar.gz"
+      sha256 "61d3faff8a08471f3c91587acae5f730153f0db13eede2d42d8f0d378b552e06"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/rockscy/spell/releases/download/v0.2.0/spell_0.2.0_linux_arm64.tar.gz"
-      sha256 "84d6c151663788f80a367b58ae0a624ade06eb8fb1da3b463ee78d6024eafdc0"
+      url "https://github.com/rockscy/spell/releases/download/v0.2.1/spell_0.2.1_linux_arm64.tar.gz"
+      sha256 "71d3c3de016ef5ebe4837fab6fe0b61f2a98c418954eb2b5d46358942278b3fb"
     end
     on_intel do
-      url "https://github.com/rockscy/spell/releases/download/v0.2.0/spell_0.2.0_linux_amd64.tar.gz"
-      sha256 "684478c1b11ddd7508580c59aa99893deb848a9f13a1eeeea099244ed412e1c5"
+      url "https://github.com/rockscy/spell/releases/download/v0.2.1/spell_0.2.1_linux_amd64.tar.gz"
+      sha256 "3b54545b82928ca01e91896a2670ce78de710f696f80ef38ada894569c56eaf3"
     end
   end
 
