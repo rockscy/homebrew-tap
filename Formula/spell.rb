489d3281d88cb1208e9229dec1a65eb9950970bb
rockscy <rockscy@users.noreply.github.com>
2026-05-07 11:33:31 +0800

    spell 0.4.0: explain mode + command_not_found shell hook

--- Formula/spell.rb
+++ Formula/spell.rb
@@ -1,28 +1,28 @@
 class Spell < Formula
   desc "AI command palette for your terminal"
   homepage "https://github.com/rockscy/spell"
-  version "0.3.2"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/rockscy/spell/releases/download/v0.3.2/spell_0.3.2_darwin_arm64.tar.gz"
-      sha256 "85df6f294c2bc38919cc859c77aad4485d6d77d9f78b89c79bedc9fa223bd40e"
+      url "https://github.com/rockscy/spell/releases/download/v0.4.0/spell_0.4.0_darwin_arm64.tar.gz"
+      sha256 "8a0c9596cdde05654990aa3e82393c6e8f6f8679949579818565b64c882f2eae"
     end
     on_intel do
-      url "https://github.com/rockscy/spell/releases/download/v0.3.2/spell_0.3.2_darwin_amd64.tar.gz"
-      sha256 "7924e4e23ad9565a8fa30986d447170a2950bd94f0df7e2b27c9307cbeff75e1"
+      url "https://github.com/rockscy/spell/releases/download/v0.4.0/spell_0.4.0_darwin_amd64.tar.gz"
+      sha256 "4d68309b81620798af22f3b57fdd857cae7377b9503cb2495b32e67892561514"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/rockscy/spell/releases/download/v0.3.2/spell_0.3.2_linux_arm64.tar.gz"
-      sha256 "775fa4ea3b9b6b9a95483f213b7365a85b88ad57a80055a2e32467662e38ab4f"
+      url "https://github.com/rockscy/spell/releases/download/v0.4.0/spell_0.4.0_linux_arm64.tar.gz"
+      sha256 "c76ebf0e441ff9c42dc842c22439904786908687766798be5cc4ca9844451e9f"
     end
     on_intel do
-      url "https://github.com/rockscy/spell/releases/download/v0.3.2/spell_0.3.2_linux_amd64.tar.gz"
-      sha256 "fece3970629d565dbca725e624250ee4a3b0cb3c48b83ebafe8e657b597d050a"
+      url "https://github.com/rockscy/spell/releases/download/v0.4.0/spell_0.4.0_linux_amd64.tar.gz"
+      sha256 "6fc5d90f9134ecd4baaa7bb7319ce4b3f2670488e4d30118a353523d92aa7c05"
     end
   end
 
